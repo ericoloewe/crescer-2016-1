e39792f1006f65ff361d3641a2db509da6308f69
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-08 11:21:03 -0300

    test(Dwarf): feito mais casos de teste para o metodo getNumeroSorte, dos quais foi testado cada caso do metodo.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -12,14 +12,90 @@ import org.junit.Test;
 public class DwarfTest
 {
     @Test
-    public void dwarfNasceVivo() {
+    public void minhaSorte() 
+    {
+        Dwarf dwarf = new Dwarf("Balin");        
+        double esperado = 101.0;
+        
+        double obtido = dwarf.getNumeroSorte();
+        
+        assertTrue(esperado == obtido);
+    }
+    
+    @Test
+    public void minhaSorteComNomeMeirelesOuSeixasComAnoBissexto() 
+    {
+        Dwarf meireles = new Dwarf("Meireles", new DataTerceiraEra(1,1,2000));
+        Dwarf seixas = new Dwarf("Seixas", new DataTerceiraEra(1,1,2000));
+        double esperado = 101;
+        
+        double obtido = meireles.getNumeroSorte();        
+        assertTrue(esperado == obtido);
+        obtido = seixas.getNumeroSorte();        
+        assertTrue(esperado == obtido);
+    }
+    
+    @Test
+    public void minhaSorteComNomeMeirelesOuSeixasSemAnoBissexto() 
+    {
+        Dwarf meireles = new Dwarf("Meireles");
+        Dwarf seixas = new Dwarf("Seixas");
+        double esperado = 33;
+        
+        double obtido = meireles.getNumeroSorte();        
+        assertTrue(esperado == obtido);
+        obtido = seixas.getNumeroSorte();        
+        assertTrue(esperado == obtido);
+    }
+    
+    @Test
+    public void minhaSorteComVida90() 
+    {
+        Dwarf dwarf = new Dwarf("Balin");        
+        double esperado = 101.0;
+        
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        double obtido = dwarf.getNumeroSorte();
+        
+        assertTrue(esperado == obtido);
+    }
+    
+    @Test
+    public void minhaSorteComAnoBissexto() 
+    {
+        Dwarf dwarf = new Dwarf("Balin", new DataTerceiraEra(1,1,2000));        
+        double esperado = 101.0;
+        
+        double obtido = dwarf.getNumeroSorte();
+        
+        assertTrue(esperado == obtido);
+    }
+    
+    @Test
+    public void minhaSorteComAnoBissextoEVida90() 
+    {
+        Dwarf dwarf = new Dwarf("Balin", new DataTerceiraEra(1,1,2000));        
+        double esperado = -3333;
+        
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        double obtido = dwarf.getNumeroSorte();
+        
+        assertTrue(esperado == obtido);
+    }
+    
+    @Test
+    public void dwarfNasceVivo() 
+    {
         Dwarf dwarf = new Dwarf("Balin");
         
         assertEquals(Status.VIVO, dwarf.getStatus());
     }
     
     @Test
-    public void dwarfNasceComAniversario() {
+    public void dwarfNasceComAniversario() 
+    {
         DataTerceiraEra data = new DataTerceiraEra(1, 1, 2016);
         Dwarf dwarf = new Dwarf("Balin", data);
         
@@ -31,7 +107,7 @@ public class DwarfTest
     {
         Dwarf dwarf = new Dwarf("Joaquim");
         
-        int esperado = dwarf.getVida() - 1;
+        int esperado = dwarf.getVida() - 10;
         dwarf.perdeVida();
         int obtido = dwarf.getVida();
                 
@@ -44,7 +120,7 @@ public class DwarfTest
         Dwarf dwarf = new Dwarf("Joaquim");
         
         int esperado = dwarf.getVida() - 10;
-        dwarf.perdeVida(10);
+        dwarf.perdeVida();
         int obtido = dwarf.getVida();
                 
         assertEquals(esperado, obtido);
@@ -56,19 +132,42 @@ public class DwarfTest
         Dwarf dwarf = new Dwarf("Joaquim");
         Status esperado = Status.MORTO;
         
-        dwarf.perdeVida(dwarf.getVida());        
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        
         Status obtido = dwarf.getStatus();
         
         assertEquals(esperado, obtido);
     }
     
     @Test
-    public void dwarfComVidaMenos1()
+    public void dwarfComVidaMenos10()
     {
         Dwarf dwarf = new Dwarf("Joaquim");
         int esperado = 0;
         
-        dwarf.perdeVida(dwarf.getVida()+1);
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        dwarf.perdeVida();
+        
         int obtido = dwarf.getVida();
         
         assertEquals(esperado, obtido);
