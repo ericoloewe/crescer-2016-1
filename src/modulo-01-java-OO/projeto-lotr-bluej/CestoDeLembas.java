e3b27115e31a7fc773fb7c8ae1e42389fed17304
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-08 13:56:05 -0300

    refactor(CestoDeLembas): limpado os comentarios impostos pelo bluej

--- src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
@@ -1,29 +1,14 @@
-/**
- * Write a description of class CestoDeLembas here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CestoDeLembas
 {
-    // instance variables - replace the example below with your own
     private int quantidadeDeLembas;
 
-    /**
-     * Constructor for objects of class CestoDeLembas
-     */
     public CestoDeLembas()
     {
-        // initialise instance variables
         quantidadeDeLembas = 0;
     }
     
-    /**
-     * Constructor for objects of class CestoDeLembas
-     */
     public CestoDeLembas(int quantidadeDeLembas)
     {
-        // initialise instance variables
         this.quantidadeDeLembas = quantidadeDeLembas;
     }
     
