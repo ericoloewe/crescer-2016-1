b5cabeb87f6c5da67f6d61fe64a3928b43209a69
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-08 15:42:03 -0300

    feat(Inventario): criado o metodo getDescricoesItens, que retorna a descricao de todos os itens separadas por ",".

--- src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -19,4 +19,17 @@ public class Inventario
     {
         return this.items;
     }
+    
+    public String getDescricoesItens()
+    {
+        Item last = this.items.get(this.items.size() - 1);
+        String descricaoItens = new String("");
+        
+        for(Item item : this.items)
+        {
+            descricaoItens += item == last ? item.getDescricao() : String.format("%s,", item.getDescricao());
+        }
+        
+        return descricaoItens;
+    }
 }
