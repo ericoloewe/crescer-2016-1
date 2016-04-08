52ce31b108713ef3c5e9d9c9ba1f51f19b2d2fee
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-08 14:54:20 -0300

    refactor(Dwarf, Item, DataTerceiraEra, Cesto): removido espaçamentos e comentarios

--- src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
@@ -1,16 +1,8 @@
-
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * The test class ItemTest.
- *
- * @author  (your name)
- * @version (a version number or a date)
- */
 public class ItemTest
 {
     @Test
