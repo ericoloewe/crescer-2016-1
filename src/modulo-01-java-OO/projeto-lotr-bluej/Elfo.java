3106e8efc24488984ab3ad99d8d481ba70a44a89
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-08 13:59:59 -0300

    refactor(Elfo): removido alguns comentarios do codigo.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
@@ -1,20 +1,11 @@
-
-/**
- * Escreva a descrição da classe Elfo aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
 public class Elfo
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
     private String nome;
     private int experiencia;
     private int flechas;
     
     public Elfo()
     {
-        // inicializa variáveis de instância
         nome = "";
         experiencia = 0;
         flechas = 42;
@@ -22,7 +13,6 @@ public class Elfo
     
     public Elfo(String nome)
     {
-        // inicializa variáveis de instância
         this.nome = nome;
 		this.experiencia = 0;
         this.flechas = 42;
@@ -30,7 +20,6 @@ public class Elfo
     
     public Elfo(String nome, int flechas)
     {
-        // inicializa variáveis de instância
         this.nome = nome;
 		this.experiencia = 0;
         this.flechas = flechas;
