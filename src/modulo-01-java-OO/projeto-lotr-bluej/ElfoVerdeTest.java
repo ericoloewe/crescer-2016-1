607362c5c3175019531870ce75ee01bb73294928
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-11 17:12:43 -0300

    test(ElfoVerde):add testes para o elfo verde.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ElfoVerdeTest
+{
+    @Test
+    public void addERemoverItemNaListaDeItemsComSucesso()
+    {
+        Item espada = new Item(10, "Espada de aço valiriano");
+        Item arco = new Item(20, "Arco e Flecha de Vidro");
+        Elfo elfo = new ElfoVerde("Elfo");
+        
+        assertTrue(elfo.adicionarItemAoInventario(espada));
+        assertTrue(elfo.adicionarItemAoInventario(arco));
+        
+        assertTrue(elfo.removerItemDoInventario(espada));
+        assertTrue(elfo.removerItemDoInventario(arco));
+    }
+    
+    @Test
+    public void addERemoverItemNaListaDeItemsSemSucesso()
+    {
+        Item espada = new Item(20, "Espada");
+        Item arco = new Item(10, "Arco e Flecha");
+        Elfo elfo = new ElfoVerde("Elfo");
+        
+        assertFalse(elfo.adicionarItemAoInventario(espada));
+        assertFalse(elfo.adicionarItemAoInventario(arco));
+        
+        assertFalse(elfo.removerItemDoInventario(espada));
+        assertFalse(elfo.removerItemDoInventario(arco));
+    }
+}
