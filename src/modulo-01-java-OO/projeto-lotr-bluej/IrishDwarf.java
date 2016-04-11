399aaf47e8b5aaf769e19a2c8d304aa9ad2c7eee
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-11 16:23:06 -0300

    refactor(IrishDwarf): extendido o irish dwarf, para que ele agora, tenha o dwarf como pai, e reutilize o codigo nele

--- src/modulo-01-java-OO/projeto-lotr-bluej/IrishDwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/IrishDwarf.java
@@ -1,117 +1,13 @@
-public class IrishDwarf
-{
-    private int vida;
-    private int experiencia;
-    private String nome;
-    private Status status;
-    private Inventario inventario;
-    private DataTerceiraEra dataNascimento;
-    
+public class IrishDwarf extends Dwarf
+{    
     public IrishDwarf(String nome)
     {
-        this.vida = 110;
-        this.experiencia = 0;
-        this.nome = nome;
-        this.status = Status.VIVO;
-        this.inventario = new Inventario();
-        this.dataNascimento = new DataTerceiraEra();
+        super(nome);
     }
     
     public IrishDwarf(String nome, DataTerceiraEra dataNascimento)
     {
-        this(nome);
-        this.dataNascimento = dataNascimento;
-    }
-    
-    public void setNome(String novoNome)
-    {
-        this.nome = novoNome;
-    }
-    
-    public String getNome()
-    {
-        return this.nome;
-    }
-    
-    public int getVida()
-    {
-        return this.vida;
-    }
-    
-    public Status getStatus()
-    {
-        return this.status;
-    }
-    
-    public int getExperiencia()
-    {
-        return this.experiencia;
-    }
-    
-    public DataTerceiraEra getDataNascimento()
-    {
-        return this.dataNascimento;
-    }
-    
-    public Inventario getInventario()
-    {
-        return this.inventario;
-    }
-    
-    private void setVida(int vida)
-    {
-        if(vida >= 0) {
-            this.vida = vida;
-        } else {
-            this.vida = 0;
-        }
-    }
-    
-    public void perdeVida() 
-    {
-        this.perdeVida(10);
-    }
-    
-    private void perdeVida(int quantidade)
-    {
-        if(!this.sorteNaVida()) {
-            this.setVida(this.vida - quantidade);
-            if(this.vida == 0) {
-                this.matarIrishDwarf();
-            }
-        }
-    }
-    
-    private boolean sorteNaVida()
-    {
-        double numSorte = this.getNumeroSorte();
-        if(numSorte < 0) {
-            this.experiencia += 2;
-            return true;
-        } else if(numSorte >= 0 && numSorte <= 100) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    private void matarIrishDwarf()
-    {
-        this.status = Status.MORTO;
-    }
-    
-    public double getNumeroSorte()
-    {
-        double theNumber = 101.0;
-        
-        if(this.dataNascimento.ehBissexto() && this.vida <= 90 && this.vida >= 80) {
-            theNumber *= -33;
-        } else if (!this.dataNascimento.ehBissexto() && ("Seixas".equals(this.nome) || "Meireles".equals(this.nome))) {
-            theNumber *= 33;
-            theNumber %= 100;
-        }
-        
-        return theNumber;        
+        super(nome, dataNascimento);
     }
     
     public void tentarSorte()
@@ -135,16 +31,6 @@ public class IrishDwarf
     
     private int somaAte(int n)
     {
-        return n > 1 ? somaAte(n-1)+n : n;
-    }
-    
-    public boolean adicionarItemAoInventario(Item item)
-    {
-        return this.inventario.adicionarItem(item);
-    }
-    
-    public boolean removerItemDoInventario(Item item)
-    {
-        return this.inventario.removerItem(item);
+        return n*(n+1)/2;
     }
 }
