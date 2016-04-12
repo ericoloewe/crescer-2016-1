2070860aad4dbe5aa4bce043725a81729b07053a
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-12 10:25:54 -0300

    feat(ElfoNoturno): implementado o elfo noturno, do qual, foi colocado uma vida do tipo float, foi implementado um metodo atirar flechas(do qual, utiliza o estamorto, removervida, para gerenciar melhor o metodo) e foi estendido o mesmo do elfo,

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -0,0 +1,52 @@
+/*
+ * 7.2 Atualmente os Elfos Noturnos podem morrer atirando flechas? Justifique.
+ * Não, pois não teria como chegar no valor zero, pois não reduzimos a vida dele em valor fixo, mas sim
+ * na porcentagem de sua vida, e isso leva a reduzir infinitamente a sua vida, até o float estourar.
+ */
+public class ElfoNoturno extends Elfo
+{
+    private float vida;
+    
+    public ElfoNoturno(String nome)
+    {
+        super(nome);
+        this.vida = 100;
+    }
+    
+    public ElfoNoturno(String nome, int flechas)
+    {
+        this(nome);
+        this.flechas = flechas;
+    }
+    
+    public boolean atirarFlechas()
+    {
+        if(this.flechas > 0 && !this.morto()) {
+            this.experiencia += 3;
+            this.removerVida();
+            this.flechas--;
+            return true;
+        }
+        return false;
+    }
+    
+    private void removerVida()
+    {
+        this.vida = (this.vida*0.95f);
+        if(this.vida < 1)
+        {
+            this.vida = 0;
+            this.status = Status.MORTO;
+        }
+    }
+    
+    private boolean morto()
+    {
+        return this.status == Status.MORTO;
+    }
+    
+    public float getVida()
+    {
+        return this.vida;
+    }
+}
