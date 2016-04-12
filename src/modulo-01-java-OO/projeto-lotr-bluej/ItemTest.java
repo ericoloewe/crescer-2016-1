1382e9df46b5ad3453d66586d839c07b857aa946
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-12 15:42:57 -0300

    test(Item): criado testes para testar se o metodo equals esta funcionando

--- src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ItemTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,5 +13,31 @@ public class ItemTest
         
         assertEquals(item.getQuantidade(), 10);
         assertEquals(item.getDescricao(), "Lorem ipsum dolor sit amet");
+    }
+    
+    @Test
+    public void igualarDoisItems()
+    {
+        Item item = new Item(10, "Lorem ipsum dolor sit amet");
+        Item item2 = new Item(10, "Lorem ipsum dolor sit amet");
+        
+        assertEquals(item, item2);
+    }
+    
+    @Test
+    public void igualarDoisItemsComNull()
+    {
+        Item item = new Item(10, null);
+        Item item2 = new Item(10, null);
+        
+        assertEquals(item, item2);
+    }
+    
+    @Test
+    public void igualarDoisItemsNãoIguais()
+    {
+        Item item = new Item(10, "Lorem ipsum dolor sit amet");
+        Item item2 = new Item(20, "Lorem ipsum dolor");
+        assertThat(item, not(item2));
     }
 }
