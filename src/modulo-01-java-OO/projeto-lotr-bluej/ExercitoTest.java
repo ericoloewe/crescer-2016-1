9836e095e5e9483d05f6519d4cf729a5fd6d76b4
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-13 21:19:33 -0300

    test(Exercito): criado metodos para testarem cada caso da classe exercito

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.*;
 
 public class ExercitoTest
 {
@@ -33,5 +34,62 @@ public class ExercitoTest
         Exercito exe = new Exercito();
         
         assertTrue(exe.alistarElfo(new ElfoNoturno("Auriel")));
+    }
+    
+    @Test
+    public void elfoEhAgrupadoPorStatus()
+    {
+        int tamanhoEsperado = 1;
+        int tamanhoArrayEsperado = 3;
+        Exercito exe = new Exercito();
+        
+        exe.alistarElfo(new ElfoNoturno("Auriel"));
+        exe.alistarElfo(new ElfoVerde("Elfo2"));
+        exe.alistarElfo(new ElfoNoturno("Elfo3"));
+        
+        exe.agruparPorStatus();
+        
+        assertEquals(tamanhoArrayEsperado, exe.getExercitoAgrupadoPorStatus().get(Status.VIVO).size());
+        assertEquals(tamanhoEsperado, exe.getExercitoAgrupadoPorStatus().size());
+        assertTrue(exe.getExercitoAgrupadoPorStatus() != null);
+    }
+    
+    @Test
+    public void elfoEhAgrupadoPorStatusVazia()
+    {
+        int tamanhoEsperado = 0;
+        Exercito exe = new Exercito();
+        
+        exe.agruparPorStatus();
+        
+        assertEquals(tamanhoEsperado, exe.getExercitoAgrupadoPorStatus().size());
+    }
+    
+    @Test
+    public void buscarElfosVivos()
+    {
+        int tamanhoEsperado = 3;
+        Exercito exe = new Exercito();
+        
+        exe.alistarElfo(new ElfoNoturno("Auriel"));
+        exe.alistarElfo(new ElfoVerde("Elfo2"));
+        exe.alistarElfo(new ElfoNoturno("Elfo3"));
+        
+        ArrayList<Elfo> elfos = exe.buscar(Status.VIVO);
+        
+        assertEquals(tamanhoEsperado, elfos.size());
+        assertTrue(elfos != null);
+    }
+    
+    @Test
+    public void buscarElfosMortos()
+    {
+        Exercito exe = new Exercito();
+        
+        exe.alistarElfo(new ElfoNoturno("Auriel"));
+        
+        ArrayList<Elfo> elfos = exe.buscar(Status.MORTO);
+        
+        assertTrue(elfos == null);
     }
 }
