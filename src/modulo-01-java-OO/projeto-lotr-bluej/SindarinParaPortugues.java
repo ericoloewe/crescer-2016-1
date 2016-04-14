fe0db9e90859cf5542e9bcf2d2044d2642817a6a
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-14 16:42:33 -0300

    feat(SindarinParaPortugues): add classe e seus devidos metodos

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/SindarinParaPortugues.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class SindarinParaPortugues implements TradutorSindarin
+{
+    private HashMap<String, String> dicionario = new HashMap<String, String>()
+    {
+        {
+            put("naur", "fogo");
+            put("amar", "terra");
+            put("gwaew", "vento");
+            put("nen", "água");
+            put("gûr", "coração");
+        }
+    };
+    
+    public String traduzir(String palavraEmSindarin)
+    {
+        return dicionario.get(palavraEmSindarin);
+    }
+}
