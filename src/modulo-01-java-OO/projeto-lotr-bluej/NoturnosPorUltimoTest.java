006b066133b5bbcb7b5dd7037b83d981a2ec3ae5
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-15 15:20:16 -0300

    test(NoturnosPorUltimo): add casos de teste para a estrategia

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimoTest.java
@@ -0,0 +1,86 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class NoturnosPorUltimoTest
+{
+    @Test
+    public void atacarDwarfs()
+    {
+        Estrategia exe = this.criarExercitoComEstrategiaArteDaGuerra();
+        ArrayList<Dwarf> exeDwarfs = criarExercitoDeDwarfs(10);
+        Estrategia exeEsperado = this.criarExercitoComEstrategiaArteDaGuerra();
+        boolean elfosEstaoEmOrdem = true, acabouElfosVerdes = false;
+        
+        exe.atacar(exeDwarfs);
+        
+        for(Dwarf dwarf : exeDwarfs)
+        {
+            assertTrue(dwarf.getVida() < 110);
+        }
+        
+        for(Elfo elfo : exe.getOrdemDoUltimoAtaque())
+        {
+            if(elfo instanceof ElfoNoturno && !acabouElfosVerdes)
+            {
+                acabouElfosVerdes = true;
+            }
+            if(elfo instanceof ElfoVerde && acabouElfosVerdes)
+            {
+                elfosEstaoEmOrdem = false;
+                break;
+            }
+        }
+        
+        assertTrue(elfosEstaoEmOrdem);
+    }
+    
+    private ArrayList<Dwarf> criarExercitoDeElfos(int n)
+    {
+        ArrayList<Dwarf> exe = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++)
+        {
+            exe.add(new Dwarf("Dwarf" + i));
+        }
+        
+        return exe;
+    }
+    
+    private ArrayList<Dwarf> criarExercitoDeDwarfs(int n)
+    {
+        ArrayList<Dwarf> exe = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++)
+        {
+            exe.add(new Dwarf("Dwarf" + i));
+        }
+        
+        return exe;
+    }
+    
+    private NoturnosPorUltimo criarExercitoComEstrategiaArteDaGuerra()
+    {
+        Exercito exe = new Exercito();
+        
+        for(int i = 0; i < 10; i++)
+        {
+            exe.alistarElfo(new ElfoVerde("ElfoVerde" + i));
+            exe.alistarElfo(new ElfoNoturno("ElfoNoturno" + i));
+        }
+        
+        return new NoturnosPorUltimo(exe);
+    }
+    
+    private ElfoNoturno criarElfoMorto(String nome)
+    {
+        ElfoNoturno elfo = new ElfoNoturno(nome, 10000);
+        
+        while(elfo.getVidaElfoNoturno() != 0) 
+            elfo.atirarFlechas();
+        
+        return elfo;
+    }
+}
