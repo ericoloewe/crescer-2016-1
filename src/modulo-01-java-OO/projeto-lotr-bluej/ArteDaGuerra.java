08b487c22d171d2856ead383ad2cfff56ddf4285
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-22 15:56:05 -0300

    fix(ArteDaGuerra): ajustado metodo atacar

--- src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerra.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerra.java
@@ -2,54 +2,37 @@ import java.util.*;
 
 public class ArteDaGuerra implements Estrategia
 {
-    private Exercito exercito = new Exercito();
     private ArrayList<Elfo> exercitoOrdenado = new ArrayList<>();
-    
-    public ArteDaGuerra(Exercito exercito)
-    {
-        this.exercito = exercito;
-    }
-    
-    public Exercito getExercito()
-    {
-        return this.exercito;
-    }
     
     public ArrayList<Elfo> getOrdemDoUltimoAtaque()
     {
         return this.exercitoOrdenado;
     }
     
-    public void atacar(ArrayList<Dwarf> hordaDeDwarfs)
+    public void atacar(ArrayList<Elfo> exercito, ArrayList<Dwarf> hordaDeDwarfs)
     {
-        if(this.exercito == null)
+        if(exercito == null)
             return;         
-        this.exercitoOrdenado = ordenarExercitoParaOAtaque(hordaDeDwarfs);
+        
+        this.exercitoOrdenado = ordenarExercitoParaOAtaque(exercito, hordaDeDwarfs);
+        
         for (Elfo elfo : this.exercitoOrdenado) 
         {
             elfoAtacarDwarfs(elfo, hordaDeDwarfs);
         }
     }
     
-    private ArrayList<Elfo> ordenarExercitoParaOAtaque(ArrayList<Dwarf> hordaDeDwarfs)
+    private ArrayList<Elfo> ordenarExercitoParaOAtaque(ArrayList<Elfo> exercito, ArrayList<Dwarf> hordaDeDwarfs)
     {
-        ArrayList<Elfo> exercito = new ArrayList<>();
-        exercito.addAll(this.exercito.getExercito().values());
-        this.removerMortos(exercito);
+        this.manterCom(Status.VIVO, exercito);
         this.removeElfosNoturnosQueNaoPodemAtacar(exercito, hordaDeDwarfs.size());
         
         return exercito;
     }
     
-    private void removerMortos(ArrayList<Elfo> exercito)
-    {
-        exercito.removeAll(this.exercito.buscar(Status.MORTO) == null ? new ArrayList<Elfo>() : this.exercito.buscar(Status.MORTO));
-    }
-    
     private ArrayList<Elfo> removeElfosNoturnosQueNaoPodemAtacar(ArrayList<Elfo> exercito, int numDeDwarfs)
     {
-        float intencoes = this.exercito.getExercito().size() * numDeDwarfs;
-        int numDeElfosNoturnosQuePodemAtacar = new Double(Math.floor(intencoes * 0.3)).intValue();
+        int numDeElfosNoturnosQuePodemAtacar = (int)(exercito.size() * numDeDwarfs * 0.3);
         int numDeElfosNoturnosJaVistos = 0;
         
         for(int i = 0; i < exercito.size(); i++)
@@ -71,6 +54,15 @@ public class ArteDaGuerra implements Estrategia
         for(Dwarf dwarf : hordaDeDwarfs)
         {
             elfo.atirarFlechas(dwarf);
+        }
+    }
+    
+    private void manterCom(Status status, ArrayList<Elfo> exercito)
+    {
+        for(int i = 0; i < exercito.size(); i++)
+        {
+            if(exercito.get(i).getStatus() != status)
+                exercito.remove(i);
         }
     }
 }
