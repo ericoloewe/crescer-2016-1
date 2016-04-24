bcce72268eaee58a3c1788cfbb4703e212cef612
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-04-24 13:24:45 -0300

    feat(Exe04): feito o exercicio da aula 4

--- src/modulo-02-banco-parte1/exercicios/exe04.sql
+++ src/modulo-02-banco-parte1/exercicios/exe04.sql
@@ -28,10 +28,10 @@ Group By c.UF;
 -- listar nome do associado sua cidade e uma coluna que indique se a mesma é da região sul
 Select a.Nome, c.Nome, 
 	Case 
-	when c.UF = 'RS' or c.UF = 'SC' or c.UF = 'PR'
-		then '***'
-	else null
-End ehRegiaoSul
+		when c.UF = 'RS' or c.UF = 'SC' or c.UF = 'PR'
+			then '***'
+		else null
+	End ehRegiaoSul
 From Associado a
 Left Join Cidade c
 On a.IDCidade = c.IDCidade;
@@ -49,19 +49,60 @@ Left Join Departamento dg
 On dg.IDDepartamento = g.IDDepartamento;
 
 -- Exe06
--- listar nome do empregado, nome do gerente e o departamento de cada um
+-- fazer uma copia da tabela empregado, fazendo um reajuste de 14,5% quando o departamento ficar em SP.
 Select e.IDEmpregado,
-      e.NomeEmpregado,
-      e.Cargo,
-      e.IDGerente,
-      e.DataAdmissao,
-      e.Comissao,
-      e.IDDepartamento,
-	  Case 
-		when de.Localizacao = 'SAO PAULO'
-			then '***'
-		else e.Salario
-		End Salario
+		e.NomeEmpregado,
+		e.Cargo,
+		e.IDGerente,
+		e.DataAdmissao,
+		e.Comissao,
+		e.IDDepartamento,
+		e.Salario,
+		Case 
+			when de.Localizacao = 'SAO PAULO'
+				then Salario * 1.145
+			else e.Salario
+		End Salario_Com_Reajuste
 From Empregado e
 Left Join Departamento de
 On de.IDDepartamento = e.IDDepartamento;
+
+-- Exe07
+-- pegar a soma dos reajustes de cada salario
+Select SUM(Salario * 0.145)
+From Empregado e
+Left Join Departamento de
+On de.IDDepartamento = e.IDDepartamento
+Where de.Localizacao = 'SAO PAULO';
+
+-- Exe08
+-- listar departamento com empregado de maior salario
+Select * 
+From Departamento dep
+Left Join Empregado emp
+On dep.IDDepartamento = emp.IDDepartamento
+Where emp.Salario = (
+	Select MAX(Salario)
+	From Empregado e
+	Inner Join Departamento de
+	On de.IDDepartamento = e.IDDepartamento
+);
+
+-- Exe09
+-- listar nome associado, nome cidade, nome empregado, cidade departamento
+Select a.Nome as Nome, c.Nome as Cidade
+From Associado a
+Left Join Cidade c
+On a.IDCidade = c.IDCidade
+Union All
+Select e.NomeEmpregado, d.Localizacao
+From Empregado e
+Left Join Departamento d
+On d.IDDepartamento = e.IDDepartamento;
+
+-- Exe10
+-- listar cidades com associado relacionado
+Select c.Nome, c.UF
+From Cidade c
+Inner Join Associado a
+On a.IDCidade = c.IDCidade;
