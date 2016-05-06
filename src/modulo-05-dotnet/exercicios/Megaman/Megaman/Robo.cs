3c3a15d7717020f7038fafbc0e63ac783f40556b
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-06 17:31:19 -0300

    feat(Bass, Megaman, Protoman, Robo): comecado o exercicio 01 de .Net.

---
+++ src/modulo-05-dotnet/exercicios/Megaman/Megaman/Robo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megaman
+{
+    abstract class Robo
+    {
+        public abstract string Nome { get; }
+        public int Vida { get; protected set; }
+        protected int Ataque { get; set; }
+        protected int Defesa { get; set; }
+
+        public Robo()
+        {
+            Vida = 100;
+            Ataque = 5;
+        }
+
+        public void Atacar(Robo robo)
+        {
+
+        }
+    }
+}
