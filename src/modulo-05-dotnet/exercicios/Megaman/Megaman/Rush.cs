2094a95a6e530c8c78530d0e1db8e5a813b8f76c
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-10 13:35:34 -0300

    feat(Rush): criado o robo Rush.

---
+++ src/modulo-05-dotnet/exercicios/Megaman/Megaman/Rush.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Megaman
+{
+    class Rush : Robo, IUpgrade
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public Rush()
+        {
+            Ataque = 4;
+            Defesa = 3;
+        }        
+    }
+}
