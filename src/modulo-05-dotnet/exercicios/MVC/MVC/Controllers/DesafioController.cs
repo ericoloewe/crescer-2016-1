9f1a2548b17fae5a89ebaeadf22e8bed99af9842
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-11 13:56:29 -0300

    refactor(Desafio): removido verificação desnecessaria

--- src/modulo-05-dotnet/exercicios/MVC/MVC/Controllers/DesafioController.cs
+++ src/modulo-05-dotnet/exercicios/MVC/MVC/Controllers/DesafioController.cs
@@ -7,7 +7,7 @@ namespace MVC.Controllers
         // GET: Desafio
         public ActionResult Index(string aluno)
         {
-            ViewBag.cssAtual = aluno ?? "";
+            ViewBag.cssAtual = string.Format("~/Content/Estilos/{0}.css", aluno);
             return View();
         }
     }
