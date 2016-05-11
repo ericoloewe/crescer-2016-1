e4ea11727eb283dce55df24005bdd99ee2cabf30
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-11 10:06:06 -0300

    feat(Exe02): feito exercicio da aula 03

---
+++ src/modulo-05-dotnet/exercicios/MVC/MVC/Controllers/DesafioController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DesafioController : Controller
+    {
+        // GET: Desafio
+        public ActionResult Index(string aluno)
+        {
+            ViewBag.cssAtual = aluno;
+            return View();
+        }
+    }
+}
