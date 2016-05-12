990682da3f3aa9eaa2c13584f9f258e0c9d45ff5
Victor Ribeiro <Victor Ribeiro>
2016-05-12 11:04:20 -0300

    feat(PedidoModel): adicionar MetaData

--- src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/Models/Pedido.cs
+++ src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/Models/Pedido.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LojaNinja.Models
 {
     public class Pedido
     {
-        [DisplayName("Nome do produto")]
+        [Required(ErrorMessage = "Nome do produto não informado!")]
+        [DisplayName("Nome do produto")]        
         public string NomeProduto { get; set; }
 
+        [Required(ErrorMessage = "Valor do produto não informado!")]
         [DisplayName("Valor do produto")]
         public double ValorVenda { get; set; }
 
+        [Required(ErrorMessage = "Selecione a data de entrega desejada!")]
         [DisplayName("Data de entrega desejada")]
         public DateTime DataEntrega { get; set; }
 
+        [Required(ErrorMessage = "Forma de pagamento não informada!")]
         [DisplayName("Forma de pagamento")]
         public TipoPagamento TipoPagamento { get; set; }
 
+        [Required(ErrorMessage = "Nome do cliente não informado!")]
         [DisplayName("Nome do cliente")]
         public string NomeCliente { get; set; }
+
         public string Cidade { get; set; }
+
         public string Estado { get; set; }
     }
 }
