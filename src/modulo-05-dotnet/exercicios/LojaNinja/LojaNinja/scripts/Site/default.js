46dcce75cf7a70fc487bbd31d468b06f06feab84
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-13 11:11:56 -0300

    feat(Detalhes): criado detalhes do pedido.

--- src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/scripts/Site/default.js
+++ src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/scripts/Site/default.js
@@ -3,35 +3,20 @@
 (function() {
     window.App = window.App || {};
 
-    App.CadastroDePedidos = {
-        iniciar: function() {
+    App.Tools = {
+        iniciar: function () {
             this.buscarElementos();
             this.vincularEventos();
-            this.arrumarValidators();
         },
 
-        buscarElementos: function() {
-            this.$form = $(".cadastro-de-pedidos");
+        buscarElementos: function () {
+            this.$tooltips = $('[data-toggle="tooltip"]');
         },
 
-        vincularEventos: function() {
-            this.$form.find("#DataEntrega").datepicker({
-                format: "dd/mm/yyyy"
-            });
-        },
-
-        arrumarValidators: function() {
-            $.validator.methods.number = function (value, element) {
-                var regex = /^(\d*)(\,\d{1,2})?$/; //99999,99
-                return this.optional(element) || regex.test(value);
-            }
-            $.validator.methods.date = function (value, element) {
-                var date = new Date(value.replace(/(\d{2})[/](\d{2})[/](\d{4})/, "$2/$1/$3")); //31/12/2015
-                return this.optional(element) || !/Invalid|NaN/.test(date.toString());
-            }
-
+        vincularEventos: function () {
+            this.$tooltips.tooltip();
         }
     };
 
-    App.CadastroDePedidos.iniciar();
+    App.Tools.iniciar();
 })();
