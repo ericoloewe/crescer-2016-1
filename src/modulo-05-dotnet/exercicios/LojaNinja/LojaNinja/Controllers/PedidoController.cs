12ce8a4a589cbafa87ea44426daa89563a745c0e
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-12 21:57:33 -0300

    feat(SalvarPedido): feito CadastrarPedido.

--- src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/Controllers/PedidoController.cs
+++ src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/Controllers/PedidoController.cs
@@ -1,10 +1,14 @@
 ﻿using LojaNinja.MVC.Models;
 using System.Web.Mvc;
+using LojaNinja.Dominio;
+using LojaNinja.Repositorio;
 
 namespace LojaNinja.MVC.Controllers
 {
     public class PedidoController : Controller
     {
+        private readonly RepositorioPedidos _repositorio = new RepositorioPedidos();
+
         // GET: Pedido
         public ActionResult Index()
         {
@@ -12,15 +16,27 @@ namespace LojaNinja.MVC.Controllers
         }
 
         // GET: Pedido
-        public ActionResult Novo()
+        public ActionResult Salvar()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Novo(PedidoViewModel pedido)
+        public ActionResult Salvar(PedidoViewModel pedido)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (pedido.Id == 0)
+                    _repositorio.IncluirPedido(
+                        new Pedido(pedido.DataEntrega, pedido.NomeProduto, pedido.ValorVenda,
+                            pedido.TipoPagamento, pedido.NomeCliente, pedido.Cidade, pedido.Estado));
+                else
+                    _repositorio.AtualizarPedido(
+                        new Pedido(pedido.DataEntrega, pedido.NomeProduto, pedido.ValorVenda,
+                            pedido.TipoPagamento, pedido.NomeCliente, pedido.Cidade, pedido.Estado, pedido.Id));
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
