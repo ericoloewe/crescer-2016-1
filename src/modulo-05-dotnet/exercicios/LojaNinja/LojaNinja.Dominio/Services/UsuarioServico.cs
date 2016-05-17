f820c0252375c0a55521748453bb2c54d394250b
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-17 17:37:50 -0300

    refactor(Usuario, Pedido, Criptografia): colocado arquivos em uma pasta.

---
+++ src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja.Dominio/Services/UsuarioServico.cs
@@ -0,0 +1,27 @@
+﻿namespace LojaNinja.Dominio.Services
+{
+    public class UsuarioServico
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioServico(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public void CadastrarUsuario(Usuario usuario)
+        {
+            usuario.CriptografarSenha();
+            _usuarioRepositorio.CadastrarUsuario(usuario);
+        }
+
+        public Usuario BuscarUsuarioPorAutenticacao(string email, string senha)
+        {
+            string senhaCriptografada = CriptografiaServico.Criptografar(senha);
+
+            Usuario usuarioEncontrado = _usuarioRepositorio.BuscarUsuarioPorAutenticacao(email, senhaCriptografada);
+
+            return usuarioEncontrado;
+        }
+    }
+}
