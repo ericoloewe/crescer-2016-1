081f2f642f2c19a82c31a895757f406d223b7e17
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-17 00:16:36 -0300

    feat(Usuario, SqlDataReader): criado extension methods.

---
+++ src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja.Repositorio/Extensions/SqlDataReaderExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LojaNinja.Repositorio.Extensions
+{
+    public static class SqlDataReaderExtension
+    {
+        public static int ParseInt(this SqlDataReader dataReader, string coluna)
+        {
+            return Convert.ToInt32(dataReader[coluna]);
+        }
+    }
+}
---
+++ src/modulo-05-dotnet/exercicios/LojaNinja/LojaNinja/Extensions/UsuarioExtension.cs
@@ -0,0 +1,29 @@
+﻿using LojaNinja.Dominio;
+using LojaNinja.MVC.Models;
+
+namespace LojaNinja.MVC.Extensions
+{
+    public static class UsuarioExtension
+    {
+        public static Usuario ToUsuario(this CadastroUsuarioViewModel usuario)
+        {
+            return new Usuario(usuario.Nome, usuario.Email, usuario.Senha);
+        }
+        public static Usuario ToUsuario(this UsuarioViewModel usuario)
+        {
+            return new Usuario(usuario.Nome, usuario.Email, usuario.Senha);
+        }
+
+        public static UsuarioViewModel ToUsuarioViewModel(this Usuario usuario)
+        {
+            return new UsuarioViewModel()
+            {
+                Id = usuario.Id,
+                Email = usuario.Email,
+                Nome = usuario.Nome, 
+                Senha = usuario.Senha,
+                Permissoes = usuario.Permissoes
+            };
+        }
+    }
+}
