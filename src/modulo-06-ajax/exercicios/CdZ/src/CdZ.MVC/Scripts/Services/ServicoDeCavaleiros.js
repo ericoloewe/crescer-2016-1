5f4e998f2244807df8427bf4811a70c1c704732c
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-20 17:32:38 -0300

    feat(Cavaleiro): começado atualiza lista dos cavaleiros.

---
+++ src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Services/ServicoDeCavaleiros.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+(function () {
+    window.App = window.App || {};
+
+    App.ServicoDeCavaleiros = {
+        buscarCavaleiros: function () {
+            return $.ajax({ url: urlCavaleiroGet, type: 'GET'/*, timeout: 2000 */ });
+        }
+    };
+
+})();
--- src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/Index.js
+++ src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/Index.js
@@ -1,90 +1,151 @@
 ﻿'use strict';
+(function () {
+    window.App = window.App || {};
 
-function carregarDadosNaPagina() {
-    $.ajax({ url: urlCavaleiroGet, type: 'GET'/*, timeout: 2000 */})
-    .then(
-        function onSuccess(res) {
-            console.log(res.data);
-            var $cavaleiros = $('#cavaleiros');
-            res.data.forEach(function (cava) {
-                $cavaleiros.append(
-                    $('<li>').append(cava.Nome)
-                );
-            });
+    App.ListaDeCavaleiros = {
+        cavaleiros: new Array(),
+
+        iniciar: function () {
+            var self = this;
+            this.atualizarLista();
+            this.criarListaDeCavaleiros();
+            this.buscarElementos();
+            this.carregarCavaleiros();
+            this.vincularEventos();
+
+            setInterval(function () {
+                self.atualizarLista();
+            }, 5000);
         },
-        function onError(res) {
-            console.error(':(');
-            console.error(res);
 
-            var criarSpanComErro = function (msg) {
-                return $('<span>').text(msg).addClass('erro');
-            };
+        buscarElementos() {
+            this.$listaDeCavaleiros = $(".lista-de-cavaleiros");
+        },
 
-            $('#feedback')
-            .append(criarSpanComErro(res.status))
-            .append($('<br>'))
-            .append(criarSpanComErro(res.statusText));
+        carregarCavaleiros: function () {
+            var self = this;
+            App.ServicoDeCavaleiros.buscarCavaleiros()
+                .then(
+                    function onSuccess(res) {
+                        res.data.forEach(function (cava) {
+                            self.addCavaleiro(cava);
+                        });
+                    },
+                    function onError(res) {
+                        console.error(':(');
+                        console.error(res);
+
+                        var criarSpanComErro = function (msg) {
+                            return $('<span>').text(msg).addClass('erro');
+                        };
+
+                        $('#feedback')
+                        .append(criarSpanComErro(res.status))
+                        .append($('<br>'))
+                        .append(criarSpanComErro(res.statusText));
+                    }
+                );
+        },
+
+        criarListaDeCavaleiros: function () {
+            var self = this;
+            var $listaDeImagens = $("<div>").addClass("lista-de-cavaleiros").addClass("row");
+            $("body > .container").append($listaDeImagens);
+        },
+
+        vincularEventos: function () {
+            var self = this;
+
+            this.$listaDeCavaleiros.find("[deletar-cavaleiro]").click(function (e) {
+                if (confirm("Voce deseja realmente deletar esse cavaleiro?")) {
+                    self.deletarCavaleiro($(this).parent().data("id-cavaleiro"));
+                    $(this).parents().find("[data-id-cavaleiro=" + $(this).parent().data("id-cavaleiro") + "]").remove();
+                }
+                return e.preventDefault();
+            })
+        },
+
+        addCavaleiro: function (cavaleiro) {
+            this.cavaleiros.push(cavaleiro);
+            this.criarDivCavaleiro(cavaleiro);
+        },
+
+        deletarCavaleiro: function (idCavaleiro) {
+            //TODO:
+        },
+
+        atualizarLista: function () {
+            //TODO:
+            console.log(this.cavaleiros);
+            App.ServicoDeCavaleiros
+                .buscarCavaleiros()
+                    .done(function (data) {
+
+                    })
+                    .fail(function (data) {
+
+                    });
+        },
+
+        criarDivCavaleiro: function (cavaleiro) {
+            var self = this;
+            var $cavaleiro = $("<div>").attr("data-id-cavaleiro", cavaleiro.Id)
+                                                            .addClass("col-sm-2")
+                                                            .addClass("cavaleiro");
+            $cavaleiro.prepend($("<h4>").text(cavaleiro.Nome));
+            if (cavaleiro.Imagens.length != 0) {
+                cavaleiro.Imagens.forEach(function (imagem, i) {
+                    self.$listaDeCavaleiros.append($cavaleiro
+                                                            .prepend($("<a>").addClass("glyphicon")
+                                                                            .addClass("glyphicon-remove")
+                                                                            .attr("deletar-cavaleiro", "")
+                                                                            .attr("href", ""))
+                                                            .append($("<img>").attr("alt", cavaleiro.Nome)
+                                                                    .attr("src", imagem.url).addClass("img-thumbnail"))
+                                                    )
+                });
+            } else
+                self.$listaDeCavaleiros.append($cavaleiro);
+        },
+
+        getCavaleiroPorId: function (idCavaleiro) {
+            //TODO:
         }
-    )
-    .always(function (res) {
-        console.log('acabouuuuuuuu');
-    });
-};
-carregarDadosNaPagina();
-
-function registrarEventoDoBotao() {
-    $('#btnCriar').click(function () {
-
-        $.ajax({
-            url: urlCavaleiroPost,
-            type: 'POST',
-            data: {
-                Nome: 'Xiru ' + new Date().getTime(),
-                AlturaCm: 187,
-                Signo: 7,
-                TipoSanguineo: 1,
-                DataNascimento: new Date(),
-                Golpes: ['Cólera do Dragão', 'Cólera dos 100 dragões'],
-                LocalNascimento: {
-                    Texto: 'Beijing'
-                },
-                LocalTreinamento: {
-                    Texto: '5 picos de rosan'
-                },
-                Imagens: [{
-                    Url: 'http://images.uncyc.org/pt/3/37/Shiryumestrepokemon.jpg',
-                    IsThumb: true
-                }, {
-                    Url: 'http://images.uncyc.org/pt/thumb/5/52/Shyryugyarados.jpg/160px-Shyryugyarados.jpg',
-                    IsThumb: false
-                }]
-            }
-        });
-
-    });
-};
-registrarEventoDoBotao();
-
-
-/*.done(function (res) {
-    console.log(res.data);
-        var $cavaleiros = $('#cavaleiros');
-        res.data.forEach(function (cava) {
-            $cavaleiros.append(
-                $('<li>').append(cava.Nome)
-            );
-        });
-})
-.fail(function (res) {
-    console.error(':(');
-    console.error(res);
-
-    var criarSpanComErro = function (msg) {
-        return $('<span>').text(msg).addClass('erro');
     };
 
-    $('#feedback')
-    .append(criarSpanComErro(res.status))
-    .append($('<br>'))
-    .append(criarSpanComErro(res.statusText));
-});*/
+    function registrarEventoDoBotao() {
+        $('#btnCriar').click(function () {
+
+            $.ajax({
+                url: urlCavaleiroPost,
+                type: 'POST',
+                data: {
+                    Nome: 'Xiru ' + new Date().getTime(),
+                    AlturaCm: 187,
+                    Signo: 7,
+                    TipoSanguineo: 1,
+                    DataNascimento: new Date(),
+                    Golpes: ['Cólera do Dragão', 'Cólera dos 100 dragões'],
+                    LocalNascimento: {
+                        Texto: 'Beijing'
+                    },
+                    LocalTreinamento: {
+                        Texto: '5 picos de rosan'
+                    },
+                    Imagens: [{
+                        Url: 'http://images.uncyc.org/pt/3/37/Shiryumestrepokemon.jpg',
+                        IsThumb: true
+                    }, {
+                        Url: 'http://images.uncyc.org/pt/thumb/5/52/Shyryugyarados.jpg/160px-Shyryugyarados.jpg',
+                        IsThumb: false
+                    }]
+                }
+            });
+
+        });
+    };
+    registrarEventoDoBotao();
+
+    App.ListaDeCavaleiros.iniciar();
+
+})();
