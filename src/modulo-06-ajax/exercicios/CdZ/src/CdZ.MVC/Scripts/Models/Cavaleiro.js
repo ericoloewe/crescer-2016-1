78f0555167a5d5715c94b3c02412742d4680f394
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-22 19:08:40 -0300

    feat(Cavaleiro): add peso ao cavaleiro.

--- src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Models/Cavaleiro.js
+++ src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Models/Cavaleiro.js
@@ -1,11 +1,12 @@
 ﻿/*
- * Model - Cavaleiros
+ * Model - Cavaleiro
  */
 
-var Cavaleiro = function(id, nome, alturaCm, signo, tipoSanguineo, dataNascimento, golpes, localNascimento, localTreinamento, imagens) {
+var Cavaleiro = function (id, nome, alturaCm, pesoLb, signo, tipoSanguineo, dataNascimento, golpes, localNascimento, localTreinamento, imagens) {
     this.Id = id;
     this.Nome = nome;
     this.AlturaCm = alturaCm;
+    this.PesoLb = pesoLb;
     this.Signo = signo;
     this.TipoSanguineo = tipoSanguineo;
     this.DataNascimento = dataNascimento;
