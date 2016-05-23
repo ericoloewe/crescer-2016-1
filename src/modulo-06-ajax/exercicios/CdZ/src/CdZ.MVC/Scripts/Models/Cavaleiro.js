04ec30a50f6c71e6df3fe641c8a197cb2d3a435d
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-05-23 11:37:20 -0300

    feat(Cavaleiro): melhorado logica.

--- src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Models/Cavaleiro.js
+++ src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Models/Cavaleiro.js
@@ -2,20 +2,40 @@
  * Model - Cavaleiro
  */
 
-var Cavaleiro = function (id, nome, alturaCm, pesoLb, signo, tipoSanguineo, dataNascimento, golpes, localNascimento, localTreinamento, imagens) {
+var Cavaleiro = function (id, nome, alturaEmM, pesoEmKg, signo, tipoSanguineo, dataNascimento, golpes, localNascimento, localTreinamento, imagens) {
     this.Id = id;
     this.Nome = nome;
-    this.AlturaCm = alturaCm;
-    this.PesoLb = pesoLb;
     this.Signo = signo;
     this.TipoSanguineo = tipoSanguineo;
-    this.DataNascimento = dataNascimento;
     this.Golpes = golpes;
     this.LocalNascimento = localNascimento;
     this.LocalTreinamento = localTreinamento;
     this.Imagens = imagens;
+    this.DataNascimento = dataNascimento;
+    this.AlturaCm = alturaEmM;
+    this.PesoLb = pesoEmKg;
 };
 
 Cavaleiro.prototype = {
+    setDataNascimento: function(dataNascimentoFormatoBr) {
+        this.DataNascimento = App.Converte.deDataBrParaPadraoISO(dataNascimentoFormatoBr);
+    },
 
+    setAltura: function (alturaEmM) {
+        this.AlturaCm = App.Converte.deMetrosParaCentimetros(alturaEmM);
+    },
+
+    alturaEmMetros: function() {
+        return App.Converte.deMetrosParaCentimetros(this.AlturaCm);
+    },
+
+    setPeso: function (pesoEmKg) {
+        console.log(App.Converte.deKgParaLibras(pesoEmKg));
+        debugger;
+        this.PesoLb = App.Converte.deKgParaLibras(pesoEmKg);
+    },
+
+    pesoEmKg: function() {
+        return App.Converte.deLibrasParaKg(this.PesoLb);
+    }
 };
--- src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Services/ServicoDeConversao.js
+++ src/modulo-06-ajax/exercicios/CdZ/src/CdZ.MVC/Scripts/Services/ServicoDeConversao.js
@@ -4,44 +4,27 @@
 
     App.Converte = {
         deDataBrParaPadraoISO: function (data) {
-            var dia = data.substr(0, 2), mes = data.substr(3, 2), ano = data.substr(6, 4);
-            return new Date(Date.parse(mes + "/" + dia + "/" + ano)).toISOString();
+            if (typeof data === "string") {
+                var dia = data.substr(0, 2), mes = data.substr(3, 2), ano = data.substr(6, 4);
+                return new Date(Date.parse(mes + "/" + dia + "/" + ano)).toISOString();
+            }
+            return null;
         },
 
         deMetrosParaCentimetros: function (altura) {
             return parseFloat(altura) * 100;
         },
 
-        deKgParaLibras: function (peso) {
-            return parseFloat(peso) * 2, 20462;
+        deCentimetrosParaMetros: function (altura) {
+            return parseFloat(altura) / 100;
         },
 
-        doFormParaUmCavaleiro: function ($cavaleiro) {
-            // Obtém o objeto nativo Form através da posição 0 no objeto jQuery e cria um FormData a partir dele
-            var cavaleiro = new FormData($cavaleiro[0]);
-            var imagens = new Array();
-            $cavaleiro.find(".imagens .imagem").each(function () {
-                imagens.push(new Imagem(
-                                    undefined,
-                                    $(this).find("input[type=text]").val(),
-                                    $(this).find("input[type=checkbox]").is(":checked")
-                                )
-                            );
-            });
+        deKgParaLibras: function (peso) {
+            return parseFloat(peso) * 2.20462;
+        },
 
-            return new Cavaleiro(
-                undefined,
-                cavaleiro.get("nome"),
-                App.Converte.deMetrosParaCentimetros(cavaleiro.get("altura")),
-                App.Converte.deKgParaLibras(cavaleiro.get("peso")),
-                cavaleiro.get("signo"),
-                cavaleiro.get("tipoSanguineo"),
-                App.Converte.deDataBrParaPadraoISO(cavaleiro.get("dataNascimento")),
-                cavaleiro.getAll("golpes[]"),
-                new Local(undefined, cavaleiro.get("localNascimento")),
-                new Local(undefined, cavaleiro.get("localTreinamento")),
-                imagens
-            );
+        deLibrasParaKg: function(peso) {
+            return parseFloat(peso) / 2.20462;
         }
     };
 })();
