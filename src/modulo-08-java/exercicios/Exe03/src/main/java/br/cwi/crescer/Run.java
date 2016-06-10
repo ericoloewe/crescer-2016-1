e706c22cb5b269e2fccec9ef4f1e76047d26c7bb
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-06-09 21:49:35 -0300

    feat(MeuSqlUtils): feito exercicio 3.

---
+++ src/modulo-08-java/exercicios/Exe03/src/main/java/br/cwi/crescer/Run.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cwi.crescer;
+
+import br.cwi.crescer.repositorio.File;
+import br.cwi.crescer.util.MeuSQLUtils;
+
+/**
+ *
+ * @author Érico de Souza Loewe
+ */
+public class Run {
+    public static void main(String[] args) {
+        MeuSQLUtils sqlUtils = new MeuSQLUtils();
+        //sqlUtils.executarArquivoSql("teste.sql");
+        //sqlUtils.executarQuerySql("select * from PESSOA").forEach(a -> {a.forEach((c, p) -> {System.out.format("| %s = %s |", c, p);}); System.out.println("");});
+        sqlUtils.exportarParaArquivo("teste.csv", "PESSOA", "ID_PESSOA;NM_PESSOA");
+    }
+}
